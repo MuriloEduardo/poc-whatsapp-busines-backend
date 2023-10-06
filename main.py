30f615d865665d11dd36f2cd47e97cc9dee514a3
muriloeduardo <muriloeduardoooooo@gmail.com>
2023-10-06 18:47:27 -0300

    Add timestamps inside sended message

--- main.py
+++ main.py
@@ -66,8 +66,17 @@ async def ws(websocket: WebSocket):
     await manager.connect(websocket)
 
     try:
-        data = [json.loads(json.dumps(document, default=str))
-                for document in messages_collection.find()]
+        data = []
+        for document in messages_collection.find():
+            document_data = json.loads(json.dumps(document, default=str))
+            if "messages" in document_data:
+                for message in document_data["messages"]:
+                    timestamp = document["_id"].generation_time
+                    timestamp_datetime = timestamp.timestamp()
+                    message["timestamps"] = timestamp_datetime
+
+            data.append(document_data)
+
         data = json.dumps(data)
 
         await manager.broadcast(data)
